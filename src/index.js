0e559969cb41cd183f2812198cf0421ca9715751
Ziv Karmi <ziv.karmi@bluecore.com>
2023-02-16 00:53:29 -0500

    pluralsight commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import { fetchWithTimeout } from './services';
+import { fetchWithTimeout, fetchMovies, fetchBooks } from './services';
 const movies = require('./data/movies.json');
 
-export function fetchMovies() {
-  const resolveFunction = () => movies;
-  return fetchWithTimeout(1000).then(resolveFunction);
+export function getBooksAndMovies() {
+  return new Promise.all([
+    fetchBooks(),
+    fetchMovies()
+  ])
+    .then(([books, movies]) => ({
+      books,
+      movies
+    }))
+    .catch(error => console.log("Error fetching books and movies", error));
 }
 
-const moviePromise = fetchMovies();
-moviePromise.then(results => {
-  console.log(results);
-});
+const getBooksAndMoviesPromise = getBooksAndMovies();
+getBooksAndMoviesPromise.then( result =>
+  console.log('getBooksAndMoviesPromise', result)
+)
+.catch(error => console.log(error));
+
+export function getBooksOrMovies() {
+  return new Promise.race([
+    fetchBooks(),
+    fetchMovies()
+  ])
+  .then(results => results)
+  .catch(error => console.log("Error waiting for the promise race", error));
+}
+
+const getBooksOrMoviesPromise = getBooksOrMovies();
+getBooksOrMoviesPromise.then( result =>
+  console.log('getBooksAndMoviesPromise', result)
+)
+.catch(error => console.log(error));
