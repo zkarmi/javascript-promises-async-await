bac74308bd4ab7ba1c9572d92d0d17ff23cf45c6
Ziv Karmi <ziv.karmi@bluecore.com>
2023-02-16 15:35:30 -0500

    pluralsight commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { fetchWithTimeout, fetchMovies, fetchBooks } from './services';
+import { fetchWithTimeout, fetchMovies, fetchBooks, asyncFetchBooks, asyncFetchMovies } from './services';
 const movies = require('./data/movies.json');
 
 function getBooksAndMovies() {
@@ -34,4 +34,35 @@ function getBooksOrMovies() {
 const getBooksOrMoviesPromise = getBooksOrMovies();
 getBooksOrMoviesPromise.then( results => {
   console.log('getBooksOrMoviesPromise', results)
+});
+
+async function getBooksAndMoviesAsync() {
+  try {
+    const [books, movies] = await Promise.all([asyncFetchBooks(), asyncFetchMovies()]);
+    return await { books, movies };
+  } catch (error) {
+    console.log("Error fetching books and movies", error);
+  }
+}
+
+async function getBooksOrMoviesAsync() {
+  try {
+    const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
+    return values;
+  } catch (error) {
+    console.log("Error waiting for the promise race", error);
+  }
+}
+
+getBooksAndMoviesAsync().then(results => {
+  console.log("movies and books", {
+    movies: results.movies,
+    books: results.books
+  });
+});
+
+getBooksOrMoviesAsync().then(results => {
+  console.log("movies OR books", {
+    results
+  });
 });
