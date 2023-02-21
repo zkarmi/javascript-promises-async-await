cadcf6ee2afadc2a2292337a1450e8a292f7540b
Ziv Karmi <ziv.karmi@bluecore.com>
2023-02-21 01:35:58 -0500

    pluralsight commit

--- src/index.js
+++ src/index.js
@@ -69,3 +69,12 @@ getBooksOrMoviesAsync()
   .catch(error => {
     console.error("Error in getBooksOrMoviesAsync execution", error)
   });
+
+  const timer1 = setTimeout(() => {
+    console.log("timer 1 has finished");
+  }, 3000);
+
+  const timer2 = setTimeout(() => {
+    console.log("timer 2 has finished");
+    clearTimeout(timer1);
+  }, 2000);
